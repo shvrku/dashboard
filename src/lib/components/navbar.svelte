ff499600ee97d0f8a5e03be70c451d94a34e997c
Sharku <shvrku@gmail.com>
2023-10-28 00:08:35 +0630

    nav logo href fix

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -4,7 +4,7 @@
   </script>
 
 <nav class=" md:flex text-center justify-between p-3  backdrop-blur-[5px] backdrop-saturate-[180%] overflow-hidden fixed z-[1] top-0 w-full border-solid border-b-[1px] border-Nav" id="navbar">
-    <a href="#Hero"><div class=""><p class="text-2xl font-medium font-AHSM m-1 md:ml-24 ">SHVRKU</p></div></a>
+    <a href="/"><div class=""><p class="text-2xl font-medium font-AHSM m-1 md:ml-24 ">SHVRKU</p></div></a>
     <div class="md:flex hidden">
         <div class=" mr-24 font-mono">
             {#if $page.data.session?.user}
